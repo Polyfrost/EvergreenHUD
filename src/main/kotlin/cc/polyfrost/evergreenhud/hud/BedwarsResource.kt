097c9e7620ccc23fd18318ad87a25334de4fa42f
Redth <redth1206@proton.me>
2023-11-12 10:55:44 +0800

    bedwars resource rewrite

--- src/main/kotlin/cc/polyfrost/evergreenhud/hud/BedwarsResource.kt
+++ src/main/kotlin/cc/polyfrost/evergreenhud/hud/BedwarsResource.kt
@@ -17,6 +17,14 @@ import net.minecraft.client.renderer.RenderHelper
 import net.minecraft.init.Items
 import net.minecraft.inventory.*
 import net.minecraft.item.ItemStack
+import kotlin.collections.Map
+
+
+private val IRON = ItemStack(Items.iron_ingot)
+private val GOLD = ItemStack(Items.gold_ingot)
+private val DIAMOND = ItemStack(Items.diamond)
+private val EMERALD = ItemStack(Items.emerald)
+private var enderChest: IInventory? = null
 
 class BedwarsResource : Config(Mod("Bedwars Resource", ModType.HUD, "/assets/evergreenhud/evergreenhud.svg"), "evergreenhud/bedwarsresource.json", false) {
 
@@ -25,42 +33,47 @@ class BedwarsResource : Config(Mod("Bedwars Resource", ModType.HUD, "/assets/eve
 
     init {
         initialize()
+        EventManager.INSTANCE.register(this)
+    }
+
+    @Subscribe
+    fun onOpenContainer(e: ScreenOpenEvent) {
+        val chestGui = e.screen as? GuiChest ?: return
+        val chestContainer = chestGui.inventorySlots as? ContainerChest ?: return
+        val title = chestContainer.lowerChestInventory.displayName.unformattedText
+        if (title != "Ender Chest") return
+        enderChest = chestContainer.lowerChestInventory
+    }
+
+    @Subscribe
+    fun onWorldLoad(e: ReceivePacketEvent) {
+        if (e.packet !is
+                //#if MC>=11202
+                //$$ net.minecraft.network.play.server.SPacketJoinGame
+                //#else
+                net.minecraft.network.play.server.S01PacketJoinGame
+        //#endif
+        ) return
+        enderChest = null
     }
 
     class BedwarsResourceHud : BasicHud(true, 1920f - 400, 1080f - 21) {
-
-        init {
-            EventManager.INSTANCE.register(this)
-        }
-
-        @Transient
-        val iron = ItemStack(Items.iron_ingot)
-
-        @Transient
-        val gold = ItemStack(Items.gold_ingot)
-
-        @Transient
-        val diamond = ItemStack(Items.diamond)
-
-        @Transient
-        val emerald = ItemStack(Items.emerald)
-
-        @Switch(
+        @Checkbox(
             name = "Show Irons"
         )
         var showIron = true
 
-        @Switch(
+        @Checkbox(
             name = "Show Golds"
         )
         var showGold = true
 
-        @Switch(
+        @Checkbox(
             name = "Show Diamonds"
         )
         var showDiamond = true
 
-        @Switch(
+        @Checkbox(
             name = "Show Emeralds"
         )
         var showEmerald = true
@@ -126,82 +139,46 @@ class BedwarsResource : Config(Mod("Bedwars Resource", ModType.HUD, "/assets/eve
         @Transient
         private var size = 0
 
-        @Transient
-        private var enderChest: IInventory? = null
-
         override fun draw(matrices: UMatrixStack?, x: Float, y: Float, scale: Float, example: Boolean) {
-            draw(matrices, x, y, scale, getItems(example), example)
+            draw(x, y, scale, example)
         }
 
-        @Subscribe
-        fun onOpenContainer(e: ScreenOpenEvent) {
-            if (e.screen !is GuiChest) return
-            val chestGUI = e.screen as GuiChest?
-            val chestContainer = chestGUI!!.inventorySlots as ContainerChest
-            val title = chestContainer.lowerChestInventory.displayName.unformattedText
-            if ("Ender Chest" != title) return
-            enderChest = chestContainer.lowerChestInventory
-        }
-
-        @Subscribe
-        fun onWorldLoad(e: ReceivePacketEvent) {
-            if (e.packet !is
-                    //#if MC>=11202
-                    //$$ net.minecraft.network.play.server.SPacketJoinGame
-                    //#else
-                    net.minecraft.network.play.server.S01PacketJoinGame
-                    //#endif
-                ) return
-            enderChest = null
-        }
-
-        private fun getItems(example: Boolean) =
-            arrayListOf<ItemStack>().run {
-
-                if (showIron) add(iron)
-                if (showGold) add(gold)
-                if (showDiamond) add(diamond)
-                if (showEmerald) add(emerald)
-
+        private val shownItems: List<ItemStack>
+            get() = arrayListOf<ItemStack>().apply {
+                if (showIron) add(IRON)
+                if (showGold) add(GOLD)
+                if (showDiamond) add(DIAMOND)
+                if (showEmerald) add(EMERALD)
                 if (displayType) reverse()
-                return@run this
             }
 
-        private fun getAmount() =
-            arrayListOf<Int>().run {
-                getItems(false).forEachIndexed { i: Int, stack: ItemStack ->
-                    var amount = 0
-                    for (item in mc.thePlayer.inventory.mainInventory) {
-                        if (item != null && item.item == stack.item) {
-                            //#if MC>=11202
-                            //$$ amount += item.getCount()
-                            //#else
-                            amount += item.stackSize
-                            //#endif
-                        }
-                    }
-                    if (showEnderChest) for (slot in 0..26) {
-                        if (enderChest?.getStackInSlot(slot)?.item != stack.item) continue
-                            amount += enderChest?.getStackInSlot(slot)
-                                //#if MC>=11202
-                                //$$ ?.getCount()
-                                //#else
-                                ?.stackSize
-                                //#endif
-                                ?: 0
-                    }
-                    add(amount)
-                }
-                return@run this
+        private fun getItemAmount(item: ItemStack): Int {
+            val itemList = mc.thePlayer.inventory.mainInventory.toMutableList()
+            enderChest?.itemStackList?.let {
+                if (showEnderChest) itemList.addAll(it)
             }
 
-        private fun draw(matrices: UMatrixStack?, x: Float, y: Float, scale: Float, items: List<ItemStack>, example: Boolean) {
+            return itemList.filter {
+                it?.item == item.item
+            }.sumOf {
+                //#if MC>=11202
+                //$$ amount += item.getCount()
+                //#else
+                it.stackSize
+                //#endif
+            }
+        }
+
+        private val IInventory.itemStackList: List<ItemStack>
+            get() = (0..26).map { index -> getStackInSlot(index) }
+
+        private fun draw(x: Float, y: Float, scale: Float, example: Boolean) {
+            val itemAmountMap: Map<ItemStack, Int> = shownItems.associateWith { getItemAmount(it) }
             val iconSize = 16f
-
             val offset = iconSize + padding
-
-            val longestWidth = getAmount().maxOfOrNull { mc.fontRendererObj.getStringWidth(it.toString()) } ?: 0
-
+            val longestWidth = itemAmountMap.maxOfOrNull { (_, amount) ->
+                mc.fontRendererObj.getStringWidth(amount.toString())
+            } ?: 0
             var lastWidth = 0
 
             size = 0
@@ -209,14 +186,10 @@ class BedwarsResource : Config(Mod("Bedwars Resource", ModType.HUD, "/assets/eve
             UGraphics.GL.pushMatrix()
             UGraphics.GL.scale(scale, scale, 1f)
             UGraphics.GL.translate(x / scale, y / scale, 0f)
-            items.forEachIndexed { i: Int, stack: ItemStack ->
-
-                if (hideZero && getAmount()[i] == 0 && !example) return@forEachIndexed
-
-                val text = getAmount()[i].toString()
-
+            for ((item, amount) in itemAmountMap) {
+                if (hideZero && amount == 0 && !example) continue
+                val text = amount.toString()
                 val textWidth = mc.fontRendererObj.getStringWidth(text)
-
                 val itemY = if (type) size * offset else 0
 
                 val iconX = when (alignment) {
@@ -231,17 +204,18 @@ class BedwarsResource : Config(Mod("Bedwars Resource", ModType.HUD, "/assets/eve
 
                 RenderHelper.enableGUIStandardItemLighting()
                 mc.renderItem.zLevel = 200f
-                mc.renderItem.renderItemAndEffectIntoGUI(stack, iconX, itemY.toInt())
-                mc.renderItem.renderItemOverlayIntoGUI(mc.fontRendererObj, stack, 0, 0, "")
+                mc.renderItem.renderItemAndEffectIntoGUI(item, iconX, itemY.toInt())
+                mc.renderItem.renderItemOverlayIntoGUI(mc.fontRendererObj, item, 0, 0, "")
                 RenderHelper.disableStandardItemLighting()
-                TextRenderer.drawScaledString(text,
+                TextRenderer.drawScaledString(
+                    text,
                     textX.toFloat(),
                     itemY.toFloat() + mc.fontRendererObj.FONT_HEIGHT / 2f,
                     textColor.rgb,
                     TextRenderer.TextType.toType(textType),
                     1f
                 )
-                size ++
+                size++
                 if (!type) lastWidth += offset.toInt() + textWidth + iconPadding
             }
             UGraphics.GL.popMatrix()
@@ -253,8 +227,11 @@ class BedwarsResource : Config(Mod("Bedwars Resource", ModType.HUD, "/assets/eve
 
         override fun getHeight(scale: Float, example: Boolean): Float = actualHeight * scale
 
-        override fun shouldShow(): Boolean {
-            return super.shouldShow() && (!hideZero || (getAmount().maxOfOrNull { it } ?: 0) > 0) && (HypixelUtils.INSTANCE.isHypixel && LocrawUtil.INSTANCE.isInGame && LocrawUtil.INSTANCE.locrawInfo?.mapName?.isNotBlank() == true && LocrawUtil.INSTANCE.locrawInfo?.gameType == LocrawInfo.GameType.BEDWARS)
-        }
+        override fun shouldShow(): Boolean = super.shouldShow()
+            && (!hideZero || (shownItems.maxOfOrNull { getItemAmount(it) } ?: 0) > 0)
+            && HypixelUtils.INSTANCE.isHypixel
+            && LocrawUtil.INSTANCE.isInGame
+            && LocrawUtil.INSTANCE.locrawInfo?.mapName?.isNotBlank() == true
+            && LocrawUtil.INSTANCE.locrawInfo?.gameType == LocrawInfo.GameType.BEDWARS
     }
 }
