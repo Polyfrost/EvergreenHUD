a20bf6a12190f2900d2262b3b98763c64a33fd67
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2023-12-11 14:18:15 -0500

    add gradual fadeout

--- src/main/kotlin/org/polyfrost/evergreenhud/hud/HeldItemLore.kt
+++ src/main/kotlin/org/polyfrost/evergreenhud/hud/HeldItemLore.kt
@@ -3,15 +3,21 @@ package org.polyfrost.evergreenhud.hud
 import cc.polyfrost.oneconfig.config.Config
 import cc.polyfrost.oneconfig.config.annotations.Exclude
 import cc.polyfrost.oneconfig.config.annotations.HUD
-import cc.polyfrost.oneconfig.config.annotations.Number
 import cc.polyfrost.oneconfig.config.annotations.Switch
+import cc.polyfrost.oneconfig.config.core.OneColor
 import cc.polyfrost.oneconfig.config.data.Mod
 import cc.polyfrost.oneconfig.config.data.ModType
 import cc.polyfrost.oneconfig.hud.TextHud
+import cc.polyfrost.oneconfig.libs.universal.UGraphics
 import cc.polyfrost.oneconfig.libs.universal.UMinecraft
+import cc.polyfrost.oneconfig.renderer.NanoVGHelper
+import cc.polyfrost.oneconfig.renderer.TextRenderer
+import cc.polyfrost.oneconfig.utils.color.ColorUtils
 import cc.polyfrost.oneconfig.utils.dsl.mc
 import org.polyfrost.evergreenhud.mixins.GuiIngameAccessor
 import org.polyfrost.evergreenhud.utils.ItemStackUtils.getLore
+import kotlin.math.min
+
 
 class HeldItemLore : Config(Mod("Held Item Lore", ModType.HUD), "evergreenhud/helditemlore.json", false) {
     @HUD(name = "Main")
@@ -23,11 +29,17 @@ class HeldItemLore : Config(Mod("Held Item Lore", ModType.HUD), "evergreenhud/he
 
     class HeldItemLoreHud : TextHud(false, 50, 50) {
 
-        @Switch(name = "Fade Out (Instantly)", description = "If disabled, the Held Item Lore HUD will remain on the screen indefinitely.")
+        @Switch(name = "Fade Out", description = "If disabled, the held item lore HUD will persist indefinitely.")
         var fadeOut: Boolean = true
+
+        @Switch(name = "Instant Fade", description = "Requires \"Fade Out\" to be enabled, otherwise this toggle is ignored.")
+        var instantFade: Boolean = false
 
         @Switch(name = "Remove Empty Lines")
         var removeEmptyLines: Boolean = false
+
+        @Switch(name = "Skip Item Name")
+        var skipItemName: Boolean = false
 
         // @Number(name = "Extra Seconds of Held Item Lore", min = 0F, max = 60F, description = "The number of extra seconds the tooltip will remain on screen before fading out.")
         // var extraSeconds = 0
@@ -35,18 +47,75 @@ class HeldItemLore : Config(Mod("Held Item Lore", ModType.HUD), "evergreenhud/he
 
         @Exclude private var opacity = 0 // a place to start in case anyone gets around to implementing gradual fadeout -ery
 
-        @Exclude private val TOOLTIP = mutableListOf(
+        @Exclude private final val TOOLTIP: MutableList<String> = mutableListOf(
             "§bExample Item Lore §7(Left click and hold to drag this!)",
             "",
             "§7If you can read this, you're spending",
             "§cWAY §7too much time in §2Minecraft§7.",
             "",
-            "§7You need to get out of your house",
+            "§7You need to §bget out of your house",
             "§7and earn some §7§osocial points.",
             "",
             "§c§lGet out there §r§7and live your life.",
             "§7Make friends. Go on a hike. §7§nDo both at once!",
         )
+
+        fun drawLine(line: String?, x: Float, y: Float, c: OneColor, scale: Float) {
+            val color = OneColor(
+                ColorUtils.setAlpha(
+                    c.rgb,
+                    min(c.alpha.toDouble(), opacity.toDouble()).toInt()
+                ) or (this.opacity shl 24)
+            )
+            UGraphics.enableBlend()
+            TextRenderer.drawScaledString(line, x, y, color.rgb, TextRenderer.TextType.toType(textType), scale)
+            UGraphics.disableBlend()
+        }
+
+        override fun drawBackground(x: Float, y: Float, width: Float, height: Float, scale: Float) {
+            val nanoVGHelper = NanoVGHelper.INSTANCE
+            nanoVGHelper.setupAndDraw(true) { vg: Long ->
+                val bgColor =
+                    ColorUtils.setAlpha(
+                        bgColor.rgb,
+                        min(bgColor.alpha.toDouble(), opacity.toDouble())
+                            .toInt()
+                    )
+                val borderColor =
+                    ColorUtils.setAlpha(
+                        borderColor.rgb,
+                        min(
+                            borderColor.alpha.toDouble(),
+                            opacity.toDouble()
+                        ).toInt()
+                    )
+                if (rounded) {
+                    nanoVGHelper.drawRoundedRect(vg, x, y, width, height, bgColor, cornerRadius * scale)
+                    if (border) nanoVGHelper.drawHollowRoundRect(
+                        vg,
+                        x - borderSize * scale,
+                        y - borderSize * scale,
+                        width + borderSize * scale,
+                        height + borderSize * scale,
+                        borderColor,
+                        cornerRadius * scale,
+                        borderSize * scale
+                    )
+                } else {
+                    nanoVGHelper.drawRect(vg, x, y, width, height, bgColor)
+                    if (border) nanoVGHelper.drawHollowRoundRect(
+                        vg,
+                        x - borderSize * scale,
+                        y - borderSize * scale,
+                        width + borderSize * scale,
+                        height + borderSize * scale,
+                        borderColor,
+                        0f,
+                        borderSize * scale
+                    )
+                }
+            }
+        }
 
         override fun shouldShow(): Boolean {
             val inGameGUI: GuiIngameAccessor = UMinecraft.getMinecraft().ingameGUI as GuiIngameAccessor
@@ -58,6 +127,7 @@ class HeldItemLore : Config(Mod("Held Item Lore", ModType.HUD), "evergreenhud/he
                 if (fadeOut) (remainingTicks * 256 / 10)
                 else 255
             if (o > 255) o = 255
+            opacity = if (instantFade) 255 else o
             return o > 0 && super.shouldShow()
         }
 
@@ -75,14 +145,7 @@ class HeldItemLore : Config(Mod("Held Item Lore", ModType.HUD), "evergreenhud/he
 
             if (example) {
                 lines.clear()
-                if (!removeEmptyLines) lines.addAll(TOOLTIP)
-                else {
-                    for (line in TOOLTIP) {
-                        if (line.isNotEmpty()) {
-                            lines.add(line)
-                        }
-                    }
-                }
+                addXToY(TOOLTIP, lines)
                 return
             }
 
@@ -96,19 +159,22 @@ class HeldItemLore : Config(Mod("Held Item Lore", ModType.HUD), "evergreenhud/he
             //#endif
             val itemName = theHeldItem.displayName
             val itemLore = theHeldItem.getLore()
-            if (itemName.isNotEmpty()) lines.add(itemName)
+            if (itemName.isNotEmpty() && !skipItemName) lines.add("§r$itemName")
             if (itemLore.isNotEmpty()) {
-                if (!removeEmptyLines) lines.addAll(itemLore)
-                else {
-                    for (line in itemLore) {
-                        if (line.isNotEmpty()) {
-                            lines.add(line)
-                        }
+                addXToY(itemLore, lines)
+            }
+        }
+
+        private fun addXToY(theListToAdd: List<String>, lines: MutableList<String>) {
+            if (!removeEmptyLines) lines.addAll(theListToAdd)
+            else {
+                for (line in theListToAdd) {
+                    if (line.isNotEmpty()) {
+                        lines.add(line)
                     }
                 }
             }
         }
-
     }
 
 }
