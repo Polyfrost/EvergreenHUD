812103ee44f5d08105e0701bfbe858de85253dfc
Wyvest <wyvestbusiness@gmail.com>
2023-12-12 10:17:25 -0500

    Squashed commit of the following:
    commit 8569532e8bc716146d25608ce8f509a7403da73e
    Author: Wyvest <wyvestbusiness@gmail.com>
    Date:   Tue Dec 12 10:17:10 2023 -0500
    
        some stuff
    
    commit 299bc687e1220cdb20401aa48ee4e4a21bd9c3f2
    Author: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
    Date:   Mon Dec 11 21:38:06 2023 -0500
    
        ready to merge now
    
    commit 0334e6902e64f30c35e3deded16f58f08d63362b
    Author: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
    Date:   Mon Dec 11 20:53:33 2023 -0500
    
        changes requested by @Wyvest + and then some (still more to go)
    
    commit e82a1e2520d02fd4137e5b681c68c46b1805a69e
    Author: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
    Date:   Mon Dec 11 18:19:41 2023 -0500
    
        GRADUAL FADEOUT ADDED, TY @ImToggle
    
    commit a20bf6a12190f2900d2262b3b98763c64a33fd67
    Author: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
    Date:   Mon Dec 11 14:18:15 2023 -0500
    
        add gradual fadeout
    
    commit d304a8d363029c4783b9cd6f91961aaf26decc54
    Author: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
    Date:   Sun Dec 10 22:15:49 2023 -0500
    
        partial implementation for now
    
    commit 69fb6dad0f3d5008e775eb8bf38d479668faf57f
    Author: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
    Date:   Sun Dec 10 20:09:50 2023 -0500
    
        GuiIngameAccessor
    
    commit be339fe568a8230094de99dc6bf152337e2fbc0f
    Author: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
    Date:   Sun Dec 10 20:06:37 2023 -0500
    
        change default position
    
    commit 6c671244f3911badb2784ecb306c6fdb5677fab9
    Author: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
    Date:   Sun Dec 10 18:30:34 2023 -0500
    
        initial HeldItemLore implementation

---
+++ src/main/kotlin/org/polyfrost/evergreenhud/hud/HeldItemLore.kt
@@ -0,0 +1,177 @@
+package org.polyfrost.evergreenhud.hud
+
+import cc.polyfrost.oneconfig.config.Config
+import cc.polyfrost.oneconfig.config.annotations.*
+import cc.polyfrost.oneconfig.config.annotations.Number
+import cc.polyfrost.oneconfig.config.core.OneColor
+import cc.polyfrost.oneconfig.config.data.Mod
+import cc.polyfrost.oneconfig.config.data.ModType
+import cc.polyfrost.oneconfig.hud.TextHud
+import cc.polyfrost.oneconfig.libs.universal.UGraphics
+import cc.polyfrost.oneconfig.libs.universal.UMinecraft
+import cc.polyfrost.oneconfig.renderer.NanoVGHelper
+import cc.polyfrost.oneconfig.renderer.TextRenderer
+import cc.polyfrost.oneconfig.utils.color.ColorUtils
+import cc.polyfrost.oneconfig.utils.dsl.mc
+import org.polyfrost.evergreenhud.mixins.GuiIngameAccessor
+import org.polyfrost.evergreenhud.utils.ItemStackUtils.getLore
+import kotlin.math.min
+
+class HeldItemLore : Config(Mod("Held Item Lore", ModType.HUD), "evergreenhud/helditemlore.json", false) {
+    @HUD(name = "Main")
+    var hud = HeldItemLoreHud()
+
+    init {
+        initialize()
+    }
+
+    class HeldItemLoreHud : TextHud(true, 50, 50) {
+
+        @Switch(name = "Fade Out", description = "If disabled, the held item lore HUD will persist indefinitely.")
+        var fadeOut: Boolean = false
+
+        @Switch(name = "Remove Empty Lines")
+        var removeEmptyLines: Boolean = false
+
+        @Switch(name = "Skip Item Name")
+        var skipItemName: Boolean = false
+
+        @Number(name = "Stop After Line", min = 0F, max = 100F, description = "The HUD will stop rendering lore lines after this amount. Leave at 0 to render all lines in an item lore.\nThis setting won't count the item's display name as a line.")
+         var stopAfterLine: Int = 0
+
+         // @Number(name = "Extra Seconds", min = 0F, max = 60F, description = "The number of extra seconds the tooltip will remain on screen before fading out.")
+         // var extraSeconds: Int = 0
+        // above line is my attempt at extending the time a tooltip had before fading out -ery
+
+        @Exclude private var opacity = 0
+
+        @Exclude private val TOOLTIP: MutableList<String> = mutableListOf(
+            "§bExample Item Lore §7(Left click and hold to drag this!)",
+            "",
+            "§7If you can read this, you're spending",
+            "§cWAY §7too much time in §2Minecraft§7.",
+            "",
+            "§7You need to §bget out of your house",
+            "§7and earn some §7§osocial points.",
+            "",
+            "§c§lGet out there §r§7and live your life.",
+            "§7Make friends. Go on a hike. §7§nDo both at once!",
+        )
+
+        override fun drawLine(line: String?, x: Float, y: Float, scale: Float) {
+            val color = OneColor(
+                ColorUtils.setAlpha(
+                    this.color.rgb,
+                    min(this.color.alpha.toDouble(), opacity.toDouble()).toInt()
+                ) or (this.opacity shl 24)
+            )
+            UGraphics.enableBlend()
+            TextRenderer.drawScaledString(line, x, y, color.rgb, TextRenderer.TextType.toType(textType), scale)
+            UGraphics.disableBlend()
+        }
+
+        override fun drawBackground(x: Float, y: Float, width: Float, height: Float, scale: Float) {
+            val nanoVGHelper = NanoVGHelper.INSTANCE
+            nanoVGHelper.setupAndDraw(true) { vg: Long ->
+                val bgColor =
+                    ColorUtils.setAlpha(
+                        bgColor.rgb,
+                        min(bgColor.alpha.toDouble(), opacity.toDouble())
+                            .toInt()
+                    )
+                val borderColor =
+                    ColorUtils.setAlpha(
+                        borderColor.rgb,
+                        min(
+                            borderColor.alpha.toDouble(),
+                            opacity.toDouble()
+                        ).toInt()
+                    )
+                if (rounded) {
+                    nanoVGHelper.drawRoundedRect(vg, x, y, width, height, bgColor, cornerRadius * scale)
+                    if (border) nanoVGHelper.drawHollowRoundRect(
+                        vg,
+                        x - borderSize * scale,
+                        y - borderSize * scale,
+                        width + borderSize * scale,
+                        height + borderSize * scale,
+                        borderColor,
+                        cornerRadius * scale,
+                        borderSize * scale
+                    )
+                } else {
+                    nanoVGHelper.drawRect(vg, x, y, width, height, bgColor)
+                    if (border) nanoVGHelper.drawHollowRoundRect(
+                        vg,
+                        x - borderSize * scale,
+                        y - borderSize * scale,
+                        width + borderSize * scale,
+                        height + borderSize * scale,
+                        borderColor,
+                        0f,
+                        borderSize * scale
+                    )
+                }
+            }
+        }
+
+        override fun shouldShow(): Boolean {
+            val inGameGUI: GuiIngameAccessor = UMinecraft.getMinecraft().ingameGUI as GuiIngameAccessor
+
+            // val ticksPerSecond = 20
+            // val extraTicks = (extraSeconds * ticksPerSecond)
+            val remainingTicks = inGameGUI.getRemainingHighlightTicks() // + extraTicks
+            var o: Int =
+                if (fadeOut) (remainingTicks * 256 / 10)
+                else 255
+            if (o > 255) o = 255
+            opacity = o
+            return o > 0 && super.shouldShow()
+        }
+
+        override fun getLines(lines: MutableList<String>, example: Boolean) {
+            if (
+            //#if MC>=11202
+            //$$ mc.player
+            //#else
+            mc.thePlayer
+        //#endif
+                == null) {
+                lines.add("Unknown")
+                return
+            }
+
+            if (example) {
+                lines.clear()
+                addTextToHUD(TOOLTIP, lines)
+                return
+            }
+
+            if (!shouldShow()) return
+
+            val theHeldItem =
+                //#if MC>=11202
+                //$$ mc.player.heldItemMainhand
+                //#else
+                mc.thePlayer.heldItem
+            //#endif
+            val itemName = theHeldItem.displayName
+            val itemLore = theHeldItem.getLore()
+            if (itemName.isNotEmpty() && !skipItemName) lines.add("§r$itemName§r") // §r to avoid coloring either the inventory or enderchest huds
+            if (itemLore.isNotEmpty()) {
+                addTextToHUD(itemLore, lines)
+            }
+        }
+
+        private fun addTextToHUD(theListToAdd: List<String>, lines: MutableList<String>) {
+            var index = 0
+            for (line in theListToAdd) {
+                if (line.isEmpty() && removeEmptyLines) continue
+                lines.add(line)
+                index++
+                if (stopAfterLine in 1..index) break
+            }
+        }
+    }
+
+}
