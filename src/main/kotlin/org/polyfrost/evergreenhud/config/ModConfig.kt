3c08e7b5db2ec0fe11694728658559c2de4fcd2c
ImToggle <98242902+ImToggle@users.noreply.github.com>
2024-03-24 20:54:08 +0800

    wrong type

--- src/main/kotlin/org/polyfrost/evergreenhud/config/ModConfig.kt
+++ src/main/kotlin/org/polyfrost/evergreenhud/config/ModConfig.kt
@@ -4,5 +4,5 @@ import cc.polyfrost.oneconfig.config.Config
 import cc.polyfrost.oneconfig.config.data.*
 import org.polyfrost.evergreenhud.EvergreenHUD
 
-object ModConfig : Config(Mod(EvergreenHUD.NAME, ModType.UTIL_QOL, "/assets/evergreenhud/evergreenhud.svg"), "${EvergreenHUD.MODID}.json") {
+object ModConfig : Config(Mod(EvergreenHUD.NAME, ModType.HUD, "/assets/evergreenhud/evergreenhud.svg"), "${EvergreenHUD.MODID}.json") {
 }
