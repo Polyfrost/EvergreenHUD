d857c2931ef18251ec250952e700a2ce08b18c18
ImToggle <98242902+ImToggle@users.noreply.github.com>
2024-05-31 17:31:58 +0800

    saturation accuracy

--- src/main/kotlin/org/polyfrost/evergreenhud/hud/Saturation.kt
+++ src/main/kotlin/org/polyfrost/evergreenhud/hud/Saturation.kt
@@ -2,6 +2,8 @@ package org.polyfrost.evergreenhud.hud
 
 import org.polyfrost.evergreenhud.utils.decimalFormat
 import cc.polyfrost.oneconfig.config.annotations.HUD
+import cc.polyfrost.oneconfig.config.annotations.Slider
+import cc.polyfrost.oneconfig.config.annotations.Switch
 import cc.polyfrost.oneconfig.hud.SingleTextHud
 import cc.polyfrost.oneconfig.utils.dsl.mc
 import org.polyfrost.evergreenhud.config.HudConfig
@@ -15,8 +17,21 @@ class Saturation: HudConfig("Saturation", "evergreenhud/saturation.json", false)
     }
 
     class SaturationHud: SingleTextHud("Saturation", true, 180, 10) {
+
+        @Slider(
+            name = "Accuracy",
+            min = 0F,
+            max = 20F
+        )
+        var accuracy = 1
+
+        @Switch(
+            name = "Trailing Zeros"
+        )
+        var trailingZeros = true
+
         override fun getText(example: Boolean): String {
-            return decimalFormat(1, true).format(mc.thePlayer?.foodStats?.saturationLevel ?: 20)
+            return decimalFormat(accuracy, trailingZeros).format(mc.thePlayer?.foodStats?.saturationLevel ?: 20)
         }
     }
 }
