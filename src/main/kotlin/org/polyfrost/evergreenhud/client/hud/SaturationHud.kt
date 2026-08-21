82778236ce760de7133c4805ae02b98e1c820bee
Tellinq <59412384+Tellinq@users.noreply.github.com>
2026-08-20 21:10:30 -0400

    Added Saturation visibility range Useful if users are only wanting to see saturation when it is low, which is generally when you need to see it.

--- src/main/kotlin/org/polyfrost/evergreenhud/client/hud/SaturationHud.kt
+++ src/main/kotlin/org/polyfrost/evergreenhud/client/hud/SaturationHud.kt
@@ -3,20 +3,40 @@ package org.polyfrost.evergreenhud.client.hud
 import org.polyfrost.evergreenhud.client.SaturationChangedEvent
 import org.polyfrost.evergreenhud.client.utils.GenericNumberHud
 import org.polyfrost.evergreenhud.client.utils.SaturationTracker
+import org.polyfrost.oneconfig.api.config.v1.annotations.RangeSlider
 import org.polyfrost.oneconfig.api.event.v1.eventHandler
+import org.polyfrost.oneconfig.api.hud.v1.Hud
+import org.polyfrost.oneconfig.api.hud.v1.HudManager
 
 class SaturationHud : GenericNumberHud(
     title = "Saturation",
     category = Category.INFO,
     defaultValue = 5f,
 ) {
+    @RangeSlider(title = "Show HUD Within Saturation Range", description = "Show the Saturation HUD only within a specific range. 0-20 always shows.", min = 0F, max = 20F, step = 1F, subcategory = "Visibility")
+    private var showRange = floatArrayOf(0f, 20f)
+
     override fun setup() {
         super.setup()
         if (isReal) {
             updateWithNumber(SaturationTracker.saturation)
+            updateWhenChanged("showRange")
         }
         eventHandler { (saturation): SaturationChangedEvent ->
             updateWithNumber(saturation)
         }
+    }
+
+    override fun clone(): Hud = (super.clone() as SaturationHud).apply {
+        showRange = this@SaturationHud.showRange.copyOf()
+    }
+
+    override fun getText(): String {
+        val value = SaturationTracker.saturation
+
+        val editing = HudManager.isEditing
+        autoHidden = !editing && (value < showRange[0] || value > showRange[1])
+
+        return format(value)
     }
 }
